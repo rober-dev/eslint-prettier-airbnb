a0f9d3f908187e4dbf4c9c726cbeb72dc88a161d
Roberto Fraga <me@rober.dev>
2019-05-31 09:59:47 +0200

    Finished

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-var msg = "hola mundo";
+const msg = 'hola mundo';
 
 var x = 3;
+
+for (var x = 0; x < 100; x++) {
+  console.log('Esto es una prueba para verificar el uso de Prettier');
+}
+
+let app;
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
